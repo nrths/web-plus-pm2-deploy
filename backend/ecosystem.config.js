dd9e13c9ce2e86af7ec7855ad402dd3ef30a83bb
nrths <northnosouth@gmail.com>
2022-12-05 17:28:59 +0300

    pm2 backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPO,
