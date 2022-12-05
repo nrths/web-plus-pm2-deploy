6e1da116f0fffe84a86fd60df1f575638f2f8a7c
nrths <northnosouth@gmail.com>
2022-12-05 19:04:20 +0300

    pm2 deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,8 +19,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/`,
-      'post-deploy': 'cd ~/web-plus-pm2-deploy/backend/source/backend/ && npm i && npm run build && pm2 restart ecosystem.config.js',
+      'pre-deploy': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd ~/web-plus-pm2-deploy/backend/ && npm i && npm run build',
     },
   },
 };
